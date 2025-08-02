fe988354f7e0a2a6f45eb55bb79e356f3b9e4573
AdithyaBalak07 <adithyabalak52004@gmail.com>
2025-08-02 19:39:15 +0530

    Create left_arrow.c

---
+++ examples/display/led_matrix_8x8/left_arrow.c
@@ -0,0 +1,35 @@
+void fw_main(void)
+{
+    WRITE_REGISTER(0x40000000, 0xFF); 
+    WRITE_REGISTER(0x40000400, 0xFF); 
+    unsigned char arrow[8] = {
+        0b00010000,
+        0b00011000,
+        0b01111100,
+        0b11111100,
+        0b01111100,
+        0b00011000,
+        0b00010000,
+        0b00000000
+    };
+
+    while (1)
+    {
+        for (int repeat = 0; repeat < 1; repeat++)
+        {
+            for (int frame = 0; frame < 50; frame++) 
+            {
+                for (int row = 0; row < 8; row++)
+                {
+                    WRITE_REGISTER(0x40000008, (1 << row)); 
+                    unsigned char current = arrow[row];
+                    unsigned char rotated = (current >> 1) | (current << 7);
+                    arrow[row] = rotated;
+                    WRITE_REGISTER(0x40000408, ~rotated);
+                    delay_us(10);
+                    WRITE_REGISTER(0x40000408, 0xFF); 
+                }
+            }
+        }
+    }
+}
