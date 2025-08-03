c367b78b4aef674750a417474cf69cd215ad0bf8
CT7566 <ashlin1954@outlook.com>
2025-08-03 11:20:22 +0530

    Program to create a pulsating effect where led starts from centre and extend outwards diagonally.

---
+++ examples/display/led_matrix_8x8/pulse.c
@@ -0,0 +1,64 @@
+#include "halo.h"
+
+void fw_main(void)
+{
+    // USER CODE BEGINS
+    WRITE_REGISTER(0x40000000, 0xFF);
+    WRITE_REGISTER(0x40000400, 0xFF);  
+
+    unsigned char vals[4] = {
+        0x18,  
+        0x24,  
+        0x42,  
+        0x81   
+    };
+    
+    int i = 0;
+    int j = 0;
+    int direction = 0;
+    int count = 0;
+    // USER CODE ENDS
+    while (1)
+    {
+        if(direction == 0)
+        {
+            while(count <= 1000)
+            {
+                for(i = 0;i<=j;i++)
+                {
+                    WRITE_REGISTER(0x40000008, vals[i]);     
+                    WRITE_REGISTER(0x40000408, ~vals[i]);  
+                    delay_us(10);
+                    WRITE_REGISTER(0x40000400, 0XFF);
+                }
+                delay_us(1000);
+                WRITE_REGISTER(0x40000400, 0XFF);
+                count ++;
+            }
+            j++;
+        }
+        else
+        {
+            while(count <= 1000)
+            {
+                for(i = 0;i<=j;i++)
+                {
+                    WRITE_REGISTER(0x40000008, vals[i]);     
+                    WRITE_REGISTER(0x40000408, ~vals[i]);  
+                    delay_us(10);
+                    WRITE_REGISTER(0x40000400, 0XFF);
+                }
+                delay_us(1000);
+                WRITE_REGISTER(0x40000400, 0XFF);
+                count ++;
+            } 
+            j--;
+        }
+        count = 0;
+        if(j == 3 || j == 0)
+        {
+            direction = ~direction;
+        }
+        delay_us(10);
+    }
+}
