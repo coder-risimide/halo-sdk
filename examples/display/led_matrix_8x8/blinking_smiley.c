c7e5f2c9adcd97ac4027119bf79c4bbc91e101c4
fathiimarisha <ftma.risha05@gmail.com>
2025-08-06 23:31:04 +0530

    Updated blinking_smiley.c
    Added doxygen style header and header file inclusion

--- examples/display/led_matrix_8x8/blinking_smiley.c
+++ examples/display/led_matrix_8x8/blinking_smiley.c
@@ -1,5 +1,18 @@
-#include "halo.h"
+/**
+ * @file    blinking_smiley.c
+ * @brief   Displays a smiley face pattern that blinks every second.
+ * @author  Fathima Risha Ryaaz
+ * @date    2025-08-05
+ *
+ * @details
+ * This file contains a function that generates a smiley that blinks on and off using two 8-bit unsigned integer arrays.
+ *
+ * @note
+ * - Controller: Halo Ver 1.0 
+ * - GPIOs used: GPIO0, GPIO1
+ */
 
+#include "halo.h"
 void fw_main(void)
 {
     WRITE_REGISTER(0x40000000, 0xFF); 
