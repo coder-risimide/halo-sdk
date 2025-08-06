f6fb1869022b515d02edc26113208cba9a44ca99
fathiimarisha <ftma.risha05@gmail.com>
2025-08-06 23:33:09 +0530

    Updated scrolling_led.c
    Added doxygen style header and header file inclusion

--- examples/display/led_matrix_8x8/scrolling_led.c
+++ examples/display/led_matrix_8x8/scrolling_led.c
@@ -1,4 +1,17 @@
+/**
+ * @file    scrolling_led.c
+ * @brief   Single LED scrolls across the top row of the 8x8 LED matrix.
+ * @author  Fathima Risha Ryaaz
+ * @date    2025-08-05
+ *
+ * @details
+ * This file contains a function that generates a scrolling LED pattern across the top row of the LED matrix.
+ * @note
+ * - Controller: Halo Ver 1.0 
+ * - GPIOs used: GPIO0, GPIO1
+ */
 
+#include "halo.h"
 void fw_main(void)
 {
     WRITE_REGISTER(0x40000000, 0xFF);  
