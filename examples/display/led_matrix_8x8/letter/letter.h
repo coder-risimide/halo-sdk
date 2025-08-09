631f3de2ede7fc7e4e99f7b0c69bcc2169d1ebc2
CT7566 <ashlin1954@outlook.com>
2025-08-09 16:28:26 +0530

    Removed the header files from the folders.

--- examples/display/led_matrix_8x8/letter/letter.h
+++
@@ -1,7 +0,0 @@
-#ifndef LETTER_H
-#define LETTER_H
-
-// Function prototype
-void letter_animation(void)
-
-#endif // FIRE_WORKS_H
--- examples/display/led_matrix_8x8/pulse/pulse.h
+++
@@ -1,7 +0,0 @@
-#ifndef PULSE_H
-#define PULSE_H
-
-// Function prototype
-void pulse(void)
-
-#endif // FIRE_WORKS_H
