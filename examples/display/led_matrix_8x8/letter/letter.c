794e7f7a83130507ddf6afaec8b9ac6e2fa5cec7
CT7566 <ashlin1954@outlook.com>
2025-08-31 21:53:48 +0530

    Update letter.c

--- examples/display/led_matrix_8x8/letter/letter.c
+++ examples/display/led_matrix_8x8/letter/letter.c
@@ -32,31 +32,23 @@ void fw_main(void)
         0x81  
     };
     
-    int shift=0;
-    int frame;
-    int row;
     while (1)
     {
-        while (shift < 8)
+        for(int shift=0;shift<8;shift++)
         {
-            for (frame = 0; frame < 50; frame++) 
+            for (int frame = 0; frame < 50; frame++) 
             {   
-                for (row = 0;row < 8; row++)
+                for (int row = 0;row < 8; row++)
                 {
                     unsigned char row_mask = (1 << row);
         
-                    unsigned char col_data = ~(pattern[row] << shift);
+                    unsigned char col_data = ~( (pattern[row] << shift) & 0xFF );
         
                     WRITE_REGISTER(0x40000008, row_mask); 
                     WRITE_REGISTER(0x40000408, col_data); 
-                    delay_us(10);  
-        
-                    WRITE_REGISTER(0x40000408, 0xFF);
-                        
+                    delay_us(10);                         
                 }
-                WRITE_REGISTER(0x40000408, 0xFF);
             }
-        WRITE_REGISTER(0x40000408, 0xFF);
         shift ++;
         }
         shift = 0;
